03910a01bf0906aac0f1ce04c2b2f474e180f4bf
Anastasiia Ilicheva <89773259782@mail.ru>
2024-03-15 22:06:46 +0300

    add lesson1:task8

---
+++ Task8/Program.cs
@@ -0,0 +1,43 @@
+﻿// int a = 15.0;
+// int b = 4;
+// int result = a % b;
+// System.Console.WriteLine(result); 
+
+// double a = 15.0;
+// int b = 4;
+
+// System.Console.WriteLine(a/b); 
+
+
+// Задача 3
+// System.Console.Write(Введите трёхзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number > 99 && number < 1000)
+// {
+//     int leftDigit = number / 100;
+//     int rightDigit = number % 10;
+//     int sum = leftDigit + rightDigit;
+//     System.Console.WriteLine($"{number} => {sum}");
+// }
+// else
+// {
+//     System.Console.WriteLine("Введено не трехзначное число!");
+// }
+
+
+// Укороченный вариант
+System.Console.Write(Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number > 99 && number < 1000)
+{
+   System.Console.WriteLine($"{number} => {number / 100 + number % 10}");
+}
+else
+{
+    System.Console.WriteLine("Введено не трехзначное число!");
+}
+
+
+
