d1518e0c08ca4e3dce2969d19507772d695dbe1b
Anastasiia Ilicheva <89773259782@mail.ru>
2024-03-15 13:31:31 +0300

    add lesson1:task7

---
+++ Task7/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// ---------Реализация с циклом WHILE-----------
+// int i = -number;
+// while(i <= number)
+// {
+//  System.Console.Write(i + " ");   
+//  i++;
+// }
+
+// ---------Реализация с циклом FOR-----------
+
+for(int i = -number; i <= number; i++)
+{
+ System.Console.Write(i + " ");   
+}
